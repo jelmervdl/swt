8265376101296d0806b7610c3796c7a6eeb16a58
Jelmer <jelmer@ikhoefgeen.nl>
2013-01-23 15:18:55 +0100

    Disable non-dbpedia templates for now.

--- boxer-sparql/test.pl
+++ boxer-sparql/test.pl
@@ -72,9 +72,10 @@ known_type(person, rdf(foaf:'Person')).
 known_relation(X, lit(X)). % fallback for now.
 
 % Verb to relation mapping
-known_action(write, rdf(movie:writer)).
 known_action(write, rdf(dbpprop:author)).
-known_action(direct, rdf(movie:director)).
+known_action(direct, rdf(dbpprop:director)).
+%known_action(write, rdf(movie:writer)).
+%known_action(direct, rdf(movie:director)).
 
 % Type (pred) to relation mapping
 known_type_relation(X, rdf(dbpprop:X)). %wild guess
