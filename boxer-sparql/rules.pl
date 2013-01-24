71c5e20284c57cdcdce38501c0ae3f1c46402d65
Jelmer <jelmer@ikhoefgeen.nl>
2013-01-24 13:04:06 +0100

    Add rules to parse 'List all countries in Europe.' correctly. Started search for topic.

--- boxer-sparql/rules.pl
+++ boxer-sparql/rules.pl
@@ -9,9 +9,10 @@ rule(Pre, [triple(A, Relation, B)|Pre]) :-
 	member(pred(C, Action), Pre),
 	known_action(Action, Relation).
 
-% Find 'of' relations
+% Find 'in' and 'of' relations
 rule(Pre, [triple(B, Rel, A) | Pre1]) :-
-	select(of(A, B), Pre, Pre1),
+	member(Keyword, [in, of]),
+	select(rel(A, Keyword, B), Pre, Pre1),
 	member(type(A, Type), Pre1),
 	known_type_relation(Type, Rel).
 
@@ -20,6 +21,13 @@ rule(Pre, [triple(B, Rel, A) | Pre1]) :-
 rule(Pre, Post) :-
 	select(eq(A, B), Pre, Pre1), % removes eq/2
 	rename_instances_in_triples(B, A, Pre1, Post).
+
+% If the topic has been given a name that is the first word of
+% the sentence, drop it. It is most likely something like "Show"
+% or "List".
+rule(Pre, Post) :-
+	member(topic(Ref), Pre),
+	select(triple(Ref, rdf(rdfs:label), nameref([1001])), Pre, Post).
 
 % For equality, we rename all the equal symbols to the same symbol.
 rename_instances_in_triples(_, _, [], []).
--- boxer-sparql/test.pl
+++ boxer-sparql/test.pl
@@ -11,7 +11,7 @@ sublist(A, B) :-
 s(drs(_Ref, Drs), Y) :-
 	sl(Drs, Y).
 
-s(_:whq(_AnswerType, Drs1, _Referent, Drs2), Y) :-
+s(_:whq(_AnswerType, Drs1, Ref, Drs2), [topic(Ref) | Y]) :-
 	s(Drs1, Y1),
 	s(Drs2, Y2),
 	append(Y1, Y2, Y).
@@ -26,7 +26,7 @@ s(Tokens:named(Ref, _Sym, _NeType, _), [triple(Ref, rdf(rdfs:label), nameref(Tok
 s(_:rel(Ref1, Ref2, Sym, _), [rel(Ref1, Sym, Ref2)]) :-
 	member(Sym, [agent, patient]).
 
-s(_:rel(Ref1, Ref2, of, _), [of(Ref1, Ref2)]).
+s(_:rel(Ref1, Ref2, Keyword, _), [rel(Ref1, Keyword, Ref2)]).
 
 s(_:rel(Ref1, Ref2, Sym, _), [triple(Ref1, Rel, Ref2), rel(Ref1, Sym, Ref2)]) :-
 	\+ member(Sym, [agent, patient, of]),
@@ -36,15 +36,20 @@ s(_:pred(Ref1, Sym, n, _), [type(Ref1, Sym) | Triples]) :-
 	known_type(Sym, Resource), Triples = [triple(Ref1, rdf(rdf:type), Resource)], ! % behavior-altering cut warning
 	; Triples = [].
 
-% for testing: ignore eq.
-%s(X:eq(Ref1, Ref1), []).
+s(_:pred(Ref1, Sym, v, _), [pred(Ref1, Sym)]).
+
+s(_:pred(Ref1, topic, a, _), [topic(Ref1)]).
+
 s(_:eq(Ref1, Ref2), [eq(Ref1, Ref2)]).
 
 % also ignore the meaning of prop.
 s(_:prop(_, Drs), Y) :-
 	s(Drs, Y).
 
-s(_:pred(Ref1, Sym, v, _), [pred(Ref1, Sym)]).
+s(_:imp(Drs1, Drs2), Y) :-
+	s(Drs1, Y1),
+	s(Drs2, Y2),
+	append(Y1, Y2, Y).
 
 % sl/2 is s/2 but for lists of drs/2's.
 sl([], []).
